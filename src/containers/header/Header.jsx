28f451d07720ff3c47df8f5f3e89928854641412
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-04-19 13:52:35 -0700

    implement header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './header.css'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id="home">
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>
+          Let's Build Something Amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Discover the power of GPT-3 from OpenAI and embark on a journey to build something truly extraordinary.
+        </p>
+
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your Email Address'/>
+          <button type='button'>Get Started</button>
+        </div>
+
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt='peeople'/>
+          <p>1600 people requested access a visit in last 24hours</p>
+        </div>
+
+      </div>
+      <div className='gpt3__header-image'>
+        <img src={ai} alt='ai' />
+      </div>
+    </div>
   )
 }
 
