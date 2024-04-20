a3b444c5c440bc747be3f1c1465916626289a470
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-04-19 19:11:27 -0700

    implement cta section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './possibility.css'
+import possibilityImage from '../../assets/possibility.png'
 
 const   Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possiblity section__padding' id='possibility'>
+      <div className='gpt3__possiblity-image'>
+        <img src={possibilityImage} alt='possibility' />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>
+          Request Early Access to Get Started
+        </h4>
+        <h1 className='gradient__text'>
+          The Possibilities are beyond your Imagination
+        </h1>
+        <p>
+          Discover a world where the only limit is your imagination, and the possibilities are as infinite as the universe itself. Let your creativity soar and embark on an extraordinary journey beyond the boundaries of what you thought possible.
+        </p>
+        <h4>
+          Request Early Access to Get Started
+        </h4>
+      </div>
+    </div>
   )
 }
 
